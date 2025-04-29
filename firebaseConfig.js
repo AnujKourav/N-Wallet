1e1499d22e1517f8aff7153396d96973fa64e5e4
Anuj Kourav <anujkourav63@gmail.com>
2025-04-29 17:56:05 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,11 +3,11 @@
 // Your Firebase Project Configuration
 const firebaseConfig = {
     apiKey: "...",
-    authDomain: "nwallet-4fe43.firebaseapp.com",
-    projectId: "nwallet-4fe43",
-    storageBucket: "nwallet-4fe43.appspot.com",
-    messagingSenderId: "289530977279",
-    appId: "1:289530977279:web:4ec7386f72aad79419a5ca"
+    authDomain: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "..."
   };
   
   // Initialize Firebase
